4a21550e41768aeabbc0d5fa2f52f9b05a58acd1
jingyingyingzhao <2930256664@qq.com>
2025-01-20 00:02:56 +0800

    tmp

--- tasks/MultiAccount/account_info.md
+++ tasks/MultiAccount/account_info.md
@@ -8,25 +8,25 @@ git remote set-url origin https://github.com/shadowater/OnmyojiAutoScript.git
 
 EiICAgITMwICAhREAgICFhECAgIVEQICAjMzDP4fDP4fDP4fNEQM/h8M/h8M/h82EQT+Hxz+Hxz+HzYSBP4fHP4fHP4fNiIE/h8c/h8c/h81EQT+Hxz+Hxz+HzUSBP4fHP4fHP4fNSIE/h8c/h8c/h8REQICAhImAgICEiUCAgI=|MSDljYfmmJ8mMiDljYfmmJ8mMyDljYfmmJ8mNCDlj5jlvIImNSDlj5jlvIImNiDljYfmmJ8mNyDljYfmmJ8mOCDlj5jlvIImOSDlj5jlvIImMTAg5Y+Y5byCJjExIOWPmOW8giYxMiDlj5jlvIImMTMg5Y+Y5byCJjE0IOiCneS9rOeJueS+myDljYfmmJ8m5pa55qGIMTUm5pa55qGIMTY=|6ZWc5b2x5pig54Wn##如果想要使用此方案，可将方案码复制到剪贴板，然后在「结界卡」的「合成方案」中选择「导入方案码」进行粘贴。
 
-| 账号                | 能带 | 同心队 | 其他                      |
-| ------------------- | ---- | ------ | ------------------------- |
-| 镜影映照            | 1    |        | 大富翁差斗技荣誉兑换+契灵 |
-| 孤子流空            | 1    |        |                           |
-| 月哥账号            | 1    | 一队   |                           |
-| 柳沉安              |      | 一队   |                           |
-| 捕月亮的猫          |      | 一队   |                           |
-| 远方 ios 大号       | 1    | 二队   |                           |
-| ~~微博 孤子流空~~ |      | 二队   | 镜影映照 大狗子           |
-| 微信 孤子流空       |      | 二队   | 镜影映照 小灯             |
-| 远方 ios 小号 153   | 1    | 三队   |                           |
-| 网易 孤子流空      |      | 三队   | 孤子流空 大狗子          |
-| 老板 136 ios        |      | 三队   | 孤子流空 小灯             |
-| 锦绣账号            | 1    | 四队   |                           |
-| 老板 182 安卓       | 1    | 四队   |                           |
-| 老板136 安卓        | 1    | 四队   |                           |
-| 寮友 大号 180       | 1    | 五队   |                           |
-| ~~小青 大号~~     | 1    | 五队   | 鬼吞                      |
-| 大鹏 大号           |      | 五队   |                           |
-| 寮友小号            | 1    | 六队   |                           |
-| 小青 小号           |      | 六队   |                           |
-| 00 大号             |      | 六队   |                           |
+| 账号              | 能带 | 同心队       | 其他                      |
+| ----------------- | ---- | ------------ | ------------------------- |
+| 镜影映照          | 1    |              | 大富翁差斗技荣誉兑换+契灵 |
+| 孤子流空          | 1    |              |                           |
+| 月哥账号          | 1    | 一队长       |                           |
+| 柳沉安            |      | 一队         | 大饼一号                  |
+| 捕月亮的猫        |      | 一队         | 大饼二号                  |
+| 远方 ios 大号     | 1    | 二队长       |                           |
+| 微博 孤子流空     |      | 二队         | 红瞳                      |
+| 微信 孤子流空     |      | 二队         |                           |
+| 远方 ios 小号 153 | 1    | 三队长       |                           |
+| 网易 孤子流空    |      | 三队         |                           |
+| 老板 136 ios      |      | 三队         |                           |
+| 锦绣账号          | 1    | 四队长       |                           |
+| 老板 182 安卓     | 1    | 四队         | ~~大饼三号~~            |
+| 老板136 安卓      | 1    | 四队         | ~~大饼四号~~            |
+| 寮友 大号 180     | 1    | ~~五队长~~ |                           |
+| 小青 大号         | 1    | 五队         | 鬼吞                      |
+| 大鹏 大号         |      | 五队         |                           |
+| 寮友小号          | 1    | ~~六队长~~ |                           |
+| 小青 小号         |      | 六队         |                           |
+| 00 大号           |      | 六队         |                           |
--- tasks/MultiAccount/multi_account_daliy.py
+++ tasks/MultiAccount/multi_account_daliy.py
@@ -1,7 +1,9 @@
+from ast import Return
 import json
 import os
 from numpy import random
 from time import sleep
+from PIL import Image
 from datetime import datetime
 
 import sys
@@ -13,12 +15,17 @@ from tasks.DailyTrifles.script_task import ScriptTask as trifles_task
 from tasks.TalismanPass.script_task import ScriptTask as talisman_task
 from tasks.KekkaiUtilize.script_task import ScriptTask as kekkai_task
 from tasks.AreaBoss.script_task import ScriptTask as areaboss_task
+
+from tasks.WantedQuests.assets import WantedQuestsAssets
 from tasks.MultiAccount.assets import MultiAccountAssets
-from tasks.GameUi.page import page_main
+from tasks.GlobalGame.assets import GlobalGameAssets
+from tasks.MysteryShop.assets import MysteryShopAssets
+from tasks.GameUi.page import page_main, page_demon_encounter
 from module.config.config import Config
 from module.device.device import Device
 from module.atom.click import RuleClick
 from module.logger import logger
+from module.base.timer import Timer
 
 def switch_account_by_name(script, assets, accountName):
     """
@@ -60,7 +67,6 @@ def switch_account_by_name(script, assets, accountName):
         if script.ocr_appear_click(assets.O_ACCOUNT_GROUP_NAME):
             break
 
-
 def switch_account():
     log_out = RuleClick((27,33,65,59), (27,33,65,59))
     login.click(log_out, 1)
@@ -91,6 +97,84 @@ def switch_account():
         
     login.app_handle_login()
 
+def screenshot_wantedquests():
+    while 1:
+        demon.screenshot()
+        if demon.appear(WantedQuestsAssets.I_TRACE_ENABLE) or demon.appear(WantedQuestsAssets.I_TRACE_DISABLE):
+            break
+        if demon.appear_then_click(WantedQuestsAssets.I_WQ_SEAL, interval=1):
+            continue
+        
+    demon.screenshot() 
+    img = Image.fromarray(demon.device.image, mode='RGB')
+    img.save(f"D:\\Software\\yys\\MultiAccount\\wantedquests\\{key}_{value}.png")
+    demon.ui_click_until_disappear(GlobalGameAssets.I_UI_BACK_RED)
+    sleep(random.random()+0.5)
+
+def screenshot_mysteryshop():
+    day_of_week = datetime.now().weekday()
+    if day_of_week != 2 and day_of_week != 5:
+        logger.warning('Today is not MysteryShop day')
+    else:
+        demon.ui_click(demon.I_MAIN_GOTO_MALL ,demon.I_BACK_Y)
+        sleep(random.random()+0.5)
+        demon.ui_click(demon.I_BACK_Y ,demon.I_BACK_BLUE)
+        sleep(random.random()+0.5)
+        demon.ui_click(demon.I_BACK_Y , MysteryShopAssets.I_ME_ENTER)
+        sleep(random.random()+0.5)
+        demon.ui_click(MysteryShopAssets.I_ME_ENTER, MysteryShopAssets.I_MS_SHARE)
+        logger.info('Enter MysteryShop')
+            
+        # refresh = RuleClick((1199,533,55,54), (1199,533,55,54))
+        # have_blue = demon.appear(MysteryShopAssets.I_MS_BLUE)
+        # have_black = demon.appear(MysteryShopAssets.I_MS_BLACK)
+        # if not have_blue and not have_black:
+        #     demon.click(refresh)
+        #     demon.click(kekkai.I_UI_CONFIRM_SAMLL)
+        
+        demon.screenshot() 
+        img = Image.fromarray(demon.device.image, mode='RGB')
+        img.save(f"D:\\Software\\yys\\MultiAccount\\mysteryshop\\{key}_{value}.png")
+            
+        sleep(random.random()+0.5)
+        demon.ui_click(demon.I_BACK_Y ,demon.I_BACK_BLUE)
+        sleep(random.random()+0.5)
+        demon.ui_click(demon.I_BACK_BLUE ,demon.I_CHECK_MAIN)
+
+def lantern_task():
+
+    if not demon.check_time():
+        logger.warning('Time is not right')
+        return 0
+    
+    demon.ui_get_current_page()
+    demon.ui_goto(page_demon_encounter)
+
+    ocr_timer = Timer(0.8)
+    ocr_timer.start()
+    while 1:
+        demon.screenshot()
+        if not ocr_timer.reached():
+            continue
+        else:
+            ocr_timer.reset()
+        cu, re, total = demon.O_DE_COUNTER.ocr(demon.device.image)
+        if cu + re != total:
+            logger.warning('Lantern count error')
+            continue
+        if cu == 0 and re == 4:
+            break
+
+        if demon.appear_then_click(demon.I_DE_FIND, interval=2.5):
+            continue
+    logger.info('Lantern count success')
+    # 然后领取红色达摩
+    demon.screenshot()
+    if not demon.appear(demon.I_DE_AWARD):
+        demon.ui_get_reward(demon.I_DE_RED_DHARMA)
+    demon.wait_until_appear(demon.I_DE_AWARD)
+    demon.ui_goto(page_main)
+
 
 daliy_json = "D:\\Software\\yys\\bot\\OnmyojiAutoScript-easy-install\\tasks\\MultiAccount\\multi_daily_temp.json"
 target_json = "D:\\Software\\yys\\bot\\OnmyojiAutoScript-easy-install\\config\\multi_account.json"
@@ -107,52 +191,54 @@ restart_task.app_start()
 login = login_task(config, device)
 multi_account = MultiAccountAssets()
 account = multi_account.O_ACCOUNT
+
+demon = demon_task(config, device)
+trifles = trifles_task(config, device)
+areaboss = areaboss_task(config, device)
+talisman = talisman_task(config, device)
+
+task_list = [trifles, areaboss, talisman]
+run_task_indices = [3]
+continue_flag = True
+
+task_list = [task_list[ii] for ii in range(len(task_list)) if ii in run_task_indices]
 for key, value in account_data.items():
+    # continue
     account.name = key
     account.keyword = value
     
-    # if "小号" not in key:
-    #     continue
+    try:
+        ## switch account
+        switch_account()
 
-    ## switch account
-    switch_account()
+        lantern_task()
+                
+        ## run task 
+        for cur_task in task_list:
+            try:
+                cur_task.run()
+            except Exception as e:
+                logger.error(f"Task {cur_task} finished")
+                
+        if demon.ui_get_current_page() != page_main:
+            demon.ui_goto(page_main)
 
-    ## load config and run task
-    # demon = demon_task(config, device)
-    # try:
-    #     demon.run()
-    # except Exception as e:
-    #     demon.ui_goto(page_main)
-    #     # demon.execute_boss()
-    #     logger.error(f"Task demon encounter finished")
-    
-    # trifles = trifles_task(config, device)
-    # try:
-    #     trifles.run()
-    # except Exception as e:
-    #     logger.error(f"Task trifles finished")
-        
-    kekkai = kekkai_task(config, device)
-    try:
-        kekkai.run()
+        # 截个图，看看勾协和蓝屏黑蛋
+        value = value.replace("*", "x")
+        if not os.path.exists(f"D:\\Software\\yys\\MultiAccount\\wantedquests\\{key}_{value}.png"):
+            screenshot_wantedquests()
+        if not os.path.exists(f"D:\\Software\\yys\\MultiAccount\\mysteryshop\\{key}_{value}.png"):
+            screenshot_mysteryshop()
+            
     except Exception as e:
-        logger.error(f"Task kekkai finished")
+        logger.error(f"Account {key} failed")
+        continue_flag = True
+        input("Need human intervention...")
+
+    # 下一个账号
+    if continue_flag:
+        sleep(10+random.random()*5)
+    else:
+        input("Press Enter to continue...")
         
-    areaboss = areaboss_task(config, device)
-    try:
-        now = datetime.now()
-        if now.hour > 7:
-            areaboss.run()
-    except Exception as e:
-        logger.error(f"Task areaboss finished")
-        
-    talisman = talisman_task(config, device)
-    try:
-        talisman.run()
-    except Exception as e:
-        talisman.ui_goto(page_main)
-        logger.error(f"Task talisman finished")
-        
-    sleep(10+random.random()*5)
-    input("Press Enter to continue...")
-    
+restart_task.app_stop()
