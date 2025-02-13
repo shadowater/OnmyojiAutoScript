a5dc5efddbc9dabda6a83ff038a38f660f0f7a38
jingyingyingzhao <2930256664@qq.com>
2025-02-14 01:43:06 +0800

    fix: 修复寄养确认出现后，又点击第七个格子导致寄养失败bug

--- tasks/Component/ReplaceShikigami/replace_shikigami.py
+++ tasks/Component/ReplaceShikigami/replace_shikigami.py
@@ -96,10 +96,10 @@ class ReplaceShikigami(BaseTask, ReplaceShikigamiAssets):
 
             if self.click(click_match, interval=1.5):
                 continue
-            if self.click(_click_match[6], interval=4.5):
-                # 有的时候第七个格子被占用到寄养上去了
-                # 导致一直无法选上
-                continue
+            # if self.click(_click_match[6], interval=4.5):
+            #     # 有的时候第七个格子被占用到寄养上去了
+            #     # 导致一直无法选上
+            #     continue
             if self.appear_then_click(self.I_U_CIRCLE_ALTERNATE, interval=2.5):
                 self.appear_then_click(self.I_U_CONFIRM_ALTERNATE, interval=1.5)
                 continue
